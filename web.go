c83dbfdf82ac970131dca13c2c32d6c8b51cd389
Hariharan Gandhi <hariharan.gandhi@sap.com>
2018-06-14 16:33:44 +0200

    Ping Pong

--- web.go
+++ web.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"net/http"
+	"fmt"
 	"io"
+	"net/http"
 	"os"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
+	// http.ListenAndServe(":8090", nil)
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hi from GO Server")
+	i := time.Now().UTC().Format(time.RFC3339Nano)
+	fmt.Printf("Ping - %s\n", i)
+	io.WriteString(w, fmt.Sprintf("Pong - %s\n", i))
 }
